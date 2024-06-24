5ae3716b2137be35fbfdc58aadba8f94023c4c68
Kaumba Chinyama <kaumba@kalichipictures.com>
2024-06-24 09:24:10 -0700

    task 4-edits

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,7 +6,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER 'hbnb_test'@'localhost'
 IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT ALL ON hbnb_test_db.* TO 'hbnb_dev'@'localhost'
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost'
 WITH GRANT OPTION;
 
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
