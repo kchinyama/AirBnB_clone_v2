728fd481f2f272051d37ca397d38ad333104eba3
Kaumba Chinyama <kaumba@kalichipictures.com>
2024-07-26 23:16:37 -0700

    task 0-requirement passes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-
 """this is a demo script that starts up a flask web server
 and serves up content based on the stipulated task requirements
 for details on the task refer to the README
 """
 
 
-
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """home page with custom greeting for visitors"""
     return "Hello HBNB!"
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
