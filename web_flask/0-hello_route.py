84ac9ea3762cdc60c78619c0bbcad0057e6bdb77
Kaumba Chinyama <kaumba@kalichipictures.com>
2024-07-26 23:01:20 -0700

    task 0- if main addition

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -15,3 +15,7 @@ app = Flask(__name__)
 def hello_hbnb():
     """home page with custom greeting for visitors"""
     return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
